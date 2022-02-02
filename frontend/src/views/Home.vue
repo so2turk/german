1b566e4b5665b66a08cc9ff6767d6ea554720c15
Serhat Ozturk <so2turk@yahoo.com>
2022-02-02 17:00:34 +0100

    Initial commit for frontend (vue)

---
+++ frontend/src/views/Home.vue
@@ -0,0 +1,17 @@
+<script>
+// @ is an alias to /src
+
+export default {
+  name: "Home",
+  components: {
+
+},
+};
+</script>
+
+<template>
+  <div class="home">
+    <!-- <img alt="Vue logo" src="../assets/logo.png" /> -->
+
+  </div>
+</template>
